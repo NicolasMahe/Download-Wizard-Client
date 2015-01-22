d8b535bf634b819d6ef46d9586fb9950e55ebfd8
Nicolas Mahe <nicolas@mahe.me>
2015-01-22 21:50:43 +0100

    Improve date

--- client/app/module/downloadManager/downloadManager.js
+++ client/app/module/downloadManager/downloadManager.js
@@ -1,1 +1,1 @@
-angular.module('module_downloadManager', ['filter_bytes', 'filter.duration', 'webservice_downloadManager']);
+angular.module('module_downloadManager', ['filter_bytes', 'filter.duration', 'webservice_downloadManager', 'angular-toArrayFilter']);
